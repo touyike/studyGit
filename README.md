41d319c9702e29e63a22f57dc04c7fe1027655b6
Toutou <touyike@gmail.com>
2014-07-17 14:00:04 -0400

    init files

---
+++ README.md
@@ -0,0 +1,3 @@
+StudyGithub
+============
+test git hub for touyike and kentnf, especially for version control, and team work.
---
+++ studyGithub.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+=head1
+
+ studyGithub -- example for git hub studing
+ date: 20140717
+ author: touyike
+
+=cut
