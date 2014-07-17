3076b4ed401d6efcc4f69a16f037c6ae62245de5
Toutou <touyike@gmail.com>
2014-07-17 14:52:02 -0400

    add testing branch

---
+++ testBranch.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+=head2
+
+ create branch
+
+ git branch testing 	# create a branch named testing
+			# now still on master branch
+ git checkout testing	# switch to testing breach
+			# now you can write code on testing branch
+
+=cut
